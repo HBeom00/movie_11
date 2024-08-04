e15778697a8d83d60412fb0309fe2f57a6d8b7cb
이정곤 <jungggon@ijeong-gon-ui-MacBookPro.local>
2024-08-05 00:14:48 +0900

    junggon(08.04):상세페이지 상단구현

--- sangse.js
+++ sangse.js
@@ -1,16 +1,10 @@
-//api key 발급: 이정곤
+//api key
 const API_KEY = "609d96fd9ad77dc5f8d4e2f536aece54"
 
-//api 받을 사이트 연결
-const URL = `https://api.themoviedb.org/3/movie/533535?api_key=${API_KEY}&language=ko-KR`;
+//api datails
+const URL = `https://api.themoviedb.org/3/movie/533535?api_key=${API_KEY}&language=ko-KR`
 
-//받은 api 연결 하기
-// fetch(URL)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-
-//상세정보 2차 시도
+//상세정보 상영일,제목,평점,오버뷰
 fetch(URL)
   .then(response => response.json())
   .then(datails => {
@@ -20,6 +14,22 @@ fetch(URL)
       document.getElementById('title').innerText = datails.title;
       document.getElementById('vote_average').innerText = datails.vote_average;
       document.getElementById('overview').innerText = datails.overview;
-
   })
 
+//상세정보 스틸컷
+fetch(`https://api.themoviedb.org/3/movie/533535/images?api_key=${API_KEY}&include_image_language=null`)
+    .then(response => response.json())
+    .then(still => {
+      console.log(still.backdrops);
+      for (let i = 0; i < 5; i++) {
+        stillcut(still.backdrops[i].file_path);
+    }})
+
+function stillcut(img) {
+  const imggs = document.createElement('div');
+  imggs.className = 'stillcut'
+  imggs.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w500${img}" alt="스틸컷 이미지"/>
+  `
+  document.getElementById('still').append(imggs);
+}
