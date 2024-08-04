b927c156f96f3fbdfa08f8b5559759299aef7eb1
leeclaire07 <somii5183@gmail.com>
2024-08-04 22:06:20 +0900

    somi(240804):업로드

--- js/list.js
+++ js/list.js
@@ -1,47 +1,44 @@
-// 본인의 API 키를 넣어주셔야 합니다.
 const API_KEY = "2bf749ea958d9defe9de1793706e4f20";
-const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`;
+let nowPage = 1;
 
-fetch(URL)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.results);
-    const movies = data.results;
-    console.log(movies);
-    const movieContainer = document.getElementById("cardWrap");
-    movies.forEach((movie) => {
-      const card = createMovieCard(movie);
-      movieContainer.appendChild(card);
-    });
-    // 이후 데이터 처리
-  })
-  .catch((error) => console.error("Error:", error));
-
-function createMovieCard(movie) {
-  const card = document.createElement("div");
-  card.className = "movie-card";
-  card.innerHTML = `
+function loadMovie() {
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${nowPage}`;
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const movies = data.results;
+      console.log(movies);
+      const movieContainer = document.getElementById("cardWrap");
+      movies.forEach((movie) => {
+        const card = createMovieCard(movie);
+        movieContainer.appendChild(card);
+      });
+      const totalPage = data.total_pages;
+      if (nowPage < totalPage) {
+        let moreMovie = document.createElement("div");
+        moreMovie.id = "more-btn";
+        moreMovie.innerText = "More View";
+        document.getElementById("cardWrap").append(moreMovie);
+        document.getElementById("more-btn").addEventListener("click", function () {});
+        document.getElementById("more-btn").addEventListener("click", () => {
+          document.getElementById("more-btn").remove();
+          nowPage++;
+          loadMovie();
+        });
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+  function createMovieCard(movie) {
+    const card = document.createElement("div");
+    card.className = "movie-card";
+    card.innerHTML = `
    <div class="card-contents">
-
-   
-   
   <img class='movie-poster' src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
  <h3>${movie.title}</h3>
  </div>
   `;
-  // card.addEventListener('click', () => alert(`Movie ID: ${movie.id}`));
-  return card;
+    return card;
+  }
 }
-
-// document.getElementById('search-button').addEventListener('click', () => {
-//   const query = document.getElementById('search-input').value.toLowerCase();
-//   const movieCards = document.querySelectorAll('.movie-card');
-//   movieCards.forEach(card => {
-//     const title = card.querySelector('h3').textContent.toLowerCase();
-//     if (title.includes(query)) {
-//       card.style.display = 'block';
-//     } else {
-//       card.style.display = 'none';
-//     }
-//   });
-// });
+loadMovie();
