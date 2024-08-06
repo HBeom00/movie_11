a42ca058d2089c0ae5b6ddb32565dd4a93039d42
JunHo <68776322+PJH-FE@users.noreply.github.com>
2024-08-06 22:10:13 +0900

    junho(08.06) : Youtube api 수정

--- js/youtube.js
+++ js/youtube.js
@@ -52,10 +52,3 @@ function loadYoutube() {
   var firstScriptTag = document.getElementsByTagName("script")[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
-// Youtube API
-function loadYoutube() {
-  var tag = document.createElement("script");
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName("script")[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}
